5fa914bc4d5000e08f179942b30ca810edb12d22
RGAntu <rgantu887@gmail.com>
2025-03-31 17:14:05 +0600

    more react core concept

---
+++ src/Batsman.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react"
+
+export default function Batsman(){
+    const [runs, setRuns] = useState(0);
+    const [sixes, setSixes] = useState(0)
+    const handleSingle = () => {
+        const updatedRuns = runs + 1;
+        setRuns(updatedRuns)
+    }
+    const handleSix = () => {
+        const updateRuns = runs + 6;
+        const updateSixes = sixes + 1;
+        setSixes(updateSixes);
+        setRuns(updateRuns);
+    }
+    return(
+        <div>
+        <h3>Player: Bangla Batsman</h3>
+        <p><small>Six: {sixes}</small></p>
+        {
+            runs > 50 && <p>You Score: 50</p>
+        }
+        <h1>Score:{runs} </h1>
+        <button onClick={handleSingle}>singles</button>
+        <button>Four</button>
+        <button onClick={handleSix}>Six</button>
+
+        </div>
+    )
+}
