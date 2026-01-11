99263a04531ea9c56419bc273d8f7d3e6fadfa29
RGAntu <rgantu887@gmail.com>
2026-01-11 21:56:47 +0600

    added more components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-import Counter from './Counter'
-import Batsman from './Batsman'
+import Counter from './Counter';
+import Batsman from './Batsman';
+import Users from './Users'
+import Friends from './Friends'
+import Posts from './Posts'
+import Players from './Players';
 import './App.css'
+import { Suspense } from 'react';
 
+// const fetchUsers = fetch("https://jsonplaceholder.typicode.com/users")
+// .then(res => res.json())
+
+// const fetchFriends = async() => {
+//   const res = await fetch("https://jsonplaceholder.typicode.com/users")
+//   return res.json()
+// }
+
+// const fetchPosts = async () =>{
+//   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+//   return res.json();
+// }
 
 
 function App() {
+  // const friendsPromise = fetchFriends()
+  // const postsPromise = fetchPosts();
   function handleClick(){
     alert("I am clicked.")
   }
@@ -17,12 +36,34 @@ function App() {
     const newNum = num + 5;
     alert(newNum);
   }
+
+
+  const handleAdd10 = (num) => {
+    const num1 = num + 10;
+    alert(num1)
+
+  }
+ 
   return (
     <>
       <h3>Vite + React</h3>
 
+      {/* <Suspense fallback={<h3>loading...</h3>}>
+        <Users fetchUsers = {fetchUsers}></Users>
+      </Suspense> */}
+
+      {/* <Suspense fallback={<h3>Friends are coming for treat...</h3>}>
+        <Friends friendsPromise={friendsPromise}></Friends>
+      </Suspense> */}
+
+      {/* <Suspense fallback={<h3>Post are coming.....</h3>}>
+        <Posts postsPromise={postsPromise}></Posts>
+      </Suspense> */}
+      <Players></Players>
+
       <Counter></Counter>
       <Batsman></Batsman>
+
 
       {/* <button onclick="handleClick()">Click Me</button> */}
       <button onClick={handleClick}>Click Me</button>
@@ -35,6 +76,9 @@ function App() {
 
       <button onClick={() => alert("click 4")}>Click Me 4</button>
       <button onClick={() => handleAdd5(7)}>Click Add 5</button>
+
+      <button onClick={() => handleAdd10(10)}>click add 10</button>
+
      
     </>
   )
